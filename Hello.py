708785586f27689d8bbf0b7fc96a5149938066de
GI071 <aleksishvili@gmail.com>
2021-05-28 18:41:49 +0300

    test commited

--- Hello.py
+++
@@ -1,8 +0,0 @@
-def function(*args, **kwargs):
-    import ipdb
-    ipdb.set_trace()
-    print('Hello!')
-
-if __name__ == '__main__':
-    function(1, 2, 3, 4, 5, user = "Giorgi", password = 'Hi')
-
